1875619a735a4bb508ab583b39ab526ef52af291
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2021-12-09 19:41:16 -0600

    Conceptos básicos
    Conceptos básicos del lenguaje de programación Python.

---
+++ Hola_Mundo.py
@@ -0,0 +1,44 @@
+#IMPRIMIR TEXTO
+print("Hola mundo\n mi nombre es Kary")
+#No se cierra con ;
+#Puede hacerse un salto de línea con \n
+#en una misma línea de código.
+
+#ASIGNACIÓN DE VALORES
+numero = 10 #asigna un valor de 10
+print(numero) #imprime el valor de la variable
+print(type(numero))
+num = 18.28
+print(num)
+print(type(num)) #python se da cuenta que es de tipo FLOAT
+
+cadena = 'Kary'
+
+print(cadena)
+print(type(cadena))
+#colocar comillas en Python
+cad = "Estoy 'estudiando'"
+print(cad)
+print(type(cadena))
+
+#BOOLEANO
+valor = True
+print(valor)
+print(type(valor))
+
+#OPERACIONES ARITMÉTICAS
+num1 = 10
+num2 = 6.7
+suma = num1 + num2#operaciones con variable
+print(suma)
+operacion = num1 + num2 * 10 / 6
+print(operacion)
+res = (num1 + num2) * 10 / 6
+print("El resultado es: ",res)
+
+#TYPADO DINÁMICO
+val= 10
+print(val)
+#la misma variable puede almacenar otro valor sin problema.
+val = "Kary"
+print(val)
