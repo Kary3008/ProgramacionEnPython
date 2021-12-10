49d07406fc664578c5a340c8e45530bda1ced0ee
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2021-12-09 19:47:35 -0600

    Comentarios
    La forma de como colocar comentarios en una línea y multilínea desde Python.

---
+++ comments.py
@@ -0,0 +1,10 @@
+#Python permite colocar comentarios de esta forma
+#usando un #
+
+''''
+Comentarios multilínea jsjs
+
+EL tipado dinámico es que una misma variable
+puede contener diferentes tipos de
+datos a lo largo del programa
+'''
