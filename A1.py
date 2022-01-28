f9142db36a92f7a53d80b641299d1621ab4cd20f
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-01-28 11:32:24 -0600

    Estropeando el código.
    Conocer la estructura de los dos tipos de errores que nos muestra al tratar de ejecutar un código en Python.

--- A1.py
+++ A1.py
@@ -6,3 +6,25 @@ print("Hisssssss...")
 #Cuando se cometa un error IDLE no debería permitir que se
 #ejecute un programa, por ejemplo:
 #cuando no se le agrega un paréntesis de cierre.
+   #print("Hisssssss..."
+
+
+print("Hisssssss...")
+#Ahora bien si quitamos una letra de la palabra print
+#este no puede reconocer el error y lo compila.
+rint("Hisssssss...")
+ #pero si marca que no está definida y te muestra una sugerencia:
+#NameError: name 'rint' is not defined. Did you mean: 'print'?
+
+
+#Esto es a que la naturaleza del error es diferente y se descubre
+#de una manera diferente de la interpretación.
+
+
+#El mensaje en rojo muestra que:
+#RASTREO: ruta del código, puedes ignorarlo ya que es un código simple.
+#UBICACIÓN DEL ERROR: El nombre del archivo que contiene el error y el número de línea.
+        #NOTA: el error no siempre se encuentra en el número de línea que indica.
+#CONTENIDO DE LÍNEA ERRÓNEA: No tiene números de línea así que muestra su ubicación
+#en la esquina inferior derecha.
+#ERROR: muestra el nombre del error, una breve explicación y posible sugerencia.
