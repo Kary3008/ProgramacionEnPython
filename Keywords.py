8bcf79044b65a183854b9429afbc0b7fd417da06
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-01-28 12:44:38 -0600

    end

---
+++ Keywords.py
@@ -0,0 +1,18 @@
+#Autor: Karina Bautista Bautista
+#DATE: 28/01/2022 12:38 p.m.
+
+#ARGUMENTOS DE PALABRAS CLAVE
+
+print("Mi nombre es", "Python.", end=" ")
+print("Monty Python.")
+
+#hay una forma de hacer que la función PRINT(), cambie su
+#comportamiento, un poco: end = " "
+
+#END: termina aquí
+# = : un signo de igual
+# " ": un valor asignado a ese argumento.
+
+#Sin end
+print("Mi nombre es", "Python.")
+print("Monty Python.")
