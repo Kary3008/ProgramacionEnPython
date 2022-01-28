fd666bb73e70729a4f8d176e97b5b6dd324fdd76
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-01-28 11:14:11 -0600

    Estropear el código.

---
+++ A1.py
@@ -0,0 +1,8 @@
+#Autor: Karina Bautista Bautista
+#DATE: 28/01/2022 11:12 a.m.
+
+print("Hisssssss...")
+
+#Cuando se cometa un error IDLE no debería permitir que se
+#ejecute un programa, por ejemplo:
+#cuando no se le agrega un paréntesis de cierre.
