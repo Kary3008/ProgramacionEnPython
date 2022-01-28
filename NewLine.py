42329e56dec67b76afd3fffb28f0a66329ffc672
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-01-28 12:34:13 -0600

    Argumentos en una misma línea.

--- NewLine.py
+++ NewLine.py
@@ -8,3 +8,10 @@ print("Vino la lluvia\ny se la llevó.")
 #Si desea mostrar una diagonal invertida dentro de una cadena
 #se debe duplicar, el no hacerlo causará un error.
 print("\\")
+
+
+#ARGUMENTOS EN UNA MISMA LÍNEA.
+print("La Witsi Witsi Arañar" , "subió" , "a su telaraña.")
+
+#Al ejecutarse todos los argumentos salen en la misma línea.
+#Coloca espacios entre los argumentos emitidos por iniciativa propia.
