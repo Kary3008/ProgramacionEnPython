5c87dcd5cfe6372e90190191e748fd1374b3e9af
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-01-28 12:28:10 -0600

    New Line
    Diferentes formas de agregar un salto de línea y qué errores podrían pasar sino se escribe de manera correcta.

---
+++ NewLine.py
@@ -0,0 +1,10 @@
+#Autor: Karina Bautista Bautista
+#DATE: 28/01/2022 12:25 p.m.
+
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print() #Otra forma de dar un salto de línea.
+print("Vino la lluvia\ny se la llevó.")
+
+#Si desea mostrar una diagonal invertida dentro de una cadena
+#se debe duplicar, el no hacerlo causará un error.
+print("\\")
