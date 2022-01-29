7317d013a15d4af6e62cae946235e78373a4ef06
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-01-28 19:21:19 -0600

    Palabras clave.

--- Keywords.py
+++ Keywords.py
@@ -16,3 +16,23 @@ print("Monty Python.")
 #Sin end
 print("Mi nombre es", "Python.")
 print("Monty Python.")
+
+#La cadena asignada al argumento de la palabra
+#clave end puede ser de cualquier longitud.
+
+
+
+#SEP = separador
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+
+#Se utiliza para poder separar los argumentos
+#Puede ser también un carácter vacío.
+
+
+#ejemplo
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+
+print("Mi", "nombre", "es", sep="-¿¿¿", end="*+++++++")
+print("Monty", "Python.", sep=" ", end="* ")
